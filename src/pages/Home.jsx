31840c014541cace259b448b901a78c9fe00c2be
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-06-19 21:13:21 -0500

    get random quote

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,35 @@ import books from '../resources/books.jpg'
 import movies from '../resources/movies.jpg'
 import characters from '../resources/gandalf.jpg'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+const URL = process.env.REACT_APP_API_URL
+const TOKEN = process.env.REACT_APP_API_TOKEN
+
+const api = axios.create({
+  baseURL: URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+})
 
 function Home() {
+  const [info, setInfo] = useState([])
+
+  const fetchData = () => {
+    api.get(URL + `/quote`)
+      .then((res) => {
+        setInfo(res.data.docs[Math.floor(Math.random() * res.data.docs.length)])
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (!info) return null
+
   return (
     <div className=''>
       <div className='grid grid-cols-2 gap-8 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2'>
@@ -24,7 +51,7 @@ function Home() {
             </div>
           </div>
         </Link>
-        <Link to ='/characters'>
+        <Link to='/characters'>
           <div className='grid grid-col-2 gap-2'>
             <h2 className='flex justify-center'>Characters</h2>
             <div className='flex justify-center'>
@@ -32,12 +59,14 @@ function Home() {
             </div>
           </div>
         </Link>
-        <div className=''>
-          <h2>Random Quote</h2>
+        <div className='grid grid-col-2 gap-2'>
+          <h2 className='flex justify-center'>Random Quote</h2>
+          <div className='flex justify-center'>
+            {info.dialog}
+          </div>
         </div>
       </div>
     </div>
-
   )
 }
 
